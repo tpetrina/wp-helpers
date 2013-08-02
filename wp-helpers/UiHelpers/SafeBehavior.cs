39dfe61f34495af43f8c0b1ba5a81e9bc3932650
Toni Petrina <toni.petrina@gmail.com>
2013-08-02 16:25:23 +0200

    Updating license info

--- wp-helpers/UiHelpers/SafeBehavior.cs
+++ wp-helpers/UiHelpers/SafeBehavior.cs
@@ -1,4 +1,5 @@
 ﻿// Taken from http://wp7nl.codeplex.com/SourceControl/changeset/view/11210#Wp7nl/Wp7nl/Behaviors/SafeBehavior.cs
+//
 // The MIT License (MIT)
 // Copyright (c) 2011 Joost van Schaik, #wp7nl
 //
@@ -7,7 +8,7 @@
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE  FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
+//
 
 using System;
 using System.Windows;
